5737e770192d379e3f28e8bd45e20f2e6f48888a
tylarochele4-netizen <tylarochele4@gmail.com>
2026-04-30 13:50:52 +0300

    Delete config/index.js

--- config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development',
-    mongoUri: process.env.MONGODB_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
-    frontendUrl: process.env.FRONTEND_URL
-};
